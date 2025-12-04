b3a81e1f2659ad47b03fe5992cde264ef5d11c4d
AdorablePuddle <45329320+AdorablePuddle@users.noreply.github.com>
2025-12-04 13:44:19 +0700

    Day 4

---
+++ day/d04b.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+const int dr[8] = { -1, -1, -1,  0,  0,  1,  1,  1};
+const int dc[8] = { -1,  0,  1, -1,  1, -1,  0,  1};
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    vector<string> paper_map;
+    string input;
+    int max_width = 0, max_height = 0;
+    
+    while (cin >> input) {
+        max_width = max(max_width, (int)input.length());
+        paper_map.push_back(input);
+    }
+    max_height = (int)paper_map.size();
+
+    int result = 0;
+    int removed_this_iteration = 0;
+
+    do {
+        removed_this_iteration = 0;
+        for (int r = 0; r < max_height; r++) {
+            for (int c = 0; c < max_width; c++) {
+                // Scan every square around this one if it is a toilet paper roll.
+                if (paper_map[r][c] == '@') {
+                    int toilet_paper_counter = 0;
+                    for (int d = 0; d < 8; d++) {
+                        int nr = r + dr[d];
+                        int nc = c + dc[d];
+
+                        if (nr < 0 || nr >= max_height) continue;
+                        if (nc < 0 || nc >= max_width) continue;
+
+                        toilet_paper_counter += (paper_map[nr][nc] == '@' || paper_map[nr][nc] == 'X');
+                    }
+
+                    if (toilet_paper_counter < 4) {
+                        paper_map[r][c] = 'X'; // Marked for removal
+                        removed_this_iteration += 1;
+                    }
+                }
+            }
+        }
+        for (int r = 0; r < max_height; r++) {
+            for (int c = 0; c < max_width; c++) {
+                // If it is marked for removal, delete it.
+                if (paper_map[r][c] == 'X') {
+                    paper_map[r][c] = '.';
+                }
+            }
+        }
+        result += removed_this_iteration;
+    } while (removed_this_iteration != 0);
+    cout << result;
+}
